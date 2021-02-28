99f043cdfa8819a0886b0d485d5c2ae5f3369743
Eric Tobin <eatobin@gmail.com>
2021-02-27 18:00:57 -0600

    borrower progress

--- BorrowerTests.fs
+++ BorrowerTests.fs
@@ -16,3 +16,8 @@ let ``getMaxBooks test`` () = getMaxBooks br1 |> should equal 1
 let ``setName test`` () =
     setName br1 "Jack"
     |> should equal { name = "Jack"; maxBooks = 1 }
+
+[<Fact>]
+let ``setMaxBooks test`` () =
+    setMaxBooks br1 11
+    |> should equal { name = "Borrower1"; maxBooks = 11 }
