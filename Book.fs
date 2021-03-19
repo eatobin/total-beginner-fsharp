d6777d4fd32035d859e65261380d312ae751f401
Eric Tobin <eatobin@gmail.com>
2021-03-19 16:11:52 -0500

    bye imports

--- Book.fs
+++ Book.fs
@@ -1,12 +1,13 @@
 module total.Book
 
-open total.Borrower
 open FSharp.Json
 
 type Book =
     { title: string
       author: string
-      borrower: Borrower option }
+      borrower: Borrower.Borrower option }
+
+type JsonString = string
 
 let defaultBook =
     { title = ""
@@ -23,12 +24,12 @@ let setBorrower br bk = { bk with borrower = br }
 
 let availableString bk =
     match (getBorrower bk) with
-    | Some br -> sprintf "Checked out to %s" (getName br)
+    | Some br -> sprintf "Checked out to %s" (Borrower.getName br)
     | None -> "Available"
 
 let toString bk =
     sprintf "%s by %s; %s" (getTitle (bk)) (getAuthor (bk)) (availableString (bk))
 
-let bookJsonStringToBook (bookString: JsonString): Book = Json.deserialize<Book> bookString
+let jsonStringToBook (bookString: JsonString): Book = Json.deserialize<Book> bookString
 
 let bookToJsonString (bk: Book): JsonString = Json.serializeU bk
--- BookTests.fs
+++ BookTests.fs
@@ -2,64 +2,71 @@ module total.BookTests
 
 open Xunit
 open FsUnit.Xunit
-open total.Borrower
-open total.Book
 
-let jsonStringBk1: JsonString =
+let jsonStringBk1: Book.JsonString =
     "{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":null}"
 
-let jsonStringBk2: JsonString =
+let jsonStringBk2: Book.JsonString =
     "{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":{\"name\":\"Borrower2\",\"maxBooks\":2}}"
 
-let br2 = { name = "Borrower2"; maxBooks = 2 }
-let bk1 = bookJsonStringToBook (jsonStringBk1)
-let bk2 = setBorrower (Some(br2)) bk1
+let br2 =
+    { Borrower.name = "Borrower2"
+      Borrower.maxBooks = 2 }
+
+let bk1 = Book.jsonStringToBook (jsonStringBk1)
+let bk2 = Book.setBorrower (Some(br2)) bk1
 
 let bk3 =
-    { defaultBook with
+    { Book.defaultBook with
           title = "Title3"
           author = "Author3" }
 
 [<Fact>]
-let ``A Book should have a title`` () = getTitle bk1 |> should equal "Title1"
+let ``A Book should have a title`` () =
+    Book.getTitle bk1 |> should equal "Title1"
 
 [<Fact>]
-let ``A Book should have an author`` () = getAuthor bk1 |> should equal "Author1"
+let ``A Book should have an author`` () =
+    Book.getAuthor bk1 |> should equal "Author1"
 
 [<Fact>]
-let ``A Book could not have an borrower`` () = getBorrower bk1 |> should be null
+let ``A Book could not have an borrower`` () = Book.getBorrower bk1 |> should be null
 
 [<Fact>]
 let ``A Book could have a borrower`` () =
-    getBorrower bk2 |> Option.get |> should equal br2
+    Book.getBorrower bk2
+    |> Option.get
+    |> should equal br2
 
 [<Fact>]
-let ``A default Book should not have a borrower`` () = getBorrower bk3 |> should be null
+let ``A default Book should not have a borrower`` () = Book.getBorrower bk3 |> should be null
 
 [<Fact>]
 let ``A book should return a string "Title1 by Author1; Available"`` () =
-    toString bk1
+    Book.toString bk1
     |> should equal "Title1 by Author1; Available"
 
 [<Fact>]
 let ``A book should return a string "Title1 by Author1; Checked out to Borrower2"`` () =
-    toString bk2
+    Book.toString bk2
     |> should equal "Title1 by Author1; Checked out to Borrower2"
 
 [<Fact>]
 let ``A book without borrower should convert from JSON`` () =
-    bookJsonStringToBook jsonStringBk1
+    Book.jsonStringToBook jsonStringBk1
     |> should equal bk1
 
 [<Fact>]
 let ``A book with borrower should convert from JSON`` () =
-    bookJsonStringToBook jsonStringBk2
+    Book.jsonStringToBook jsonStringBk2
     |> should equal bk2
 
 [<Fact>]
 let ``A book without borrower should convert to JSON`` () =
-    bookToJsonString bk1 |> should equal jsonStringBk1
+    Book.bookToJsonString bk1
+    |> should equal jsonStringBk1
 
 [<Fact>]
 let ``A book with borrower should convert to JSON`` () =
-    bookToJsonString bk2 |> should equal jsonStringBk2
+    Book.bookToJsonString bk2
+    |> should equal jsonStringBk2
