dc8d89dc51806b2ed2391e4c6bcde13f3dbe7eb3
Eric Tobin <eatobin@gmail.com>
2021-03-19 22:06:00 -0500

    Library progress

--- LibraryTests.fs
+++ LibraryTests.fs
@@ -16,12 +16,38 @@ let br3 =
       Borrower.maxBooks = 3 }
 
 let brs1 = [ br1; br2 ]
-
 let brs2 = [ br3; br1; br2 ]
+
+let bk1 =
+    { Book.title = "Title1"
+      Book.author = "Author1"
+      Book.borrower = Some(br1) }
+
+let bk2 =
+    { Book.title = "Title2"
+      Book.author = "Author2"
+      Book.borrower = None }
+
+let bk3 =
+    { Book.title = "Title3"
+      Book.author = "Author3"
+      Book.borrower = Some(br3) }
+
+let bks1 = [ bk1; bk2 ]
+let bks2 = [ bk3; bk1; bk2 ]
 
 [<Fact>]
 let ``A Library should add a Borrower or Book correctly - 1`` () =
     Library.addItem br3 brs1 |> should equal brs2
+
 [<Fact>]
 let ``A Library should add a Borrower or Book correctly - 2`` () =
     Library.addItem br2 brs1 |> should equal brs1
+
+[<Fact>]
+let ``A Library should add a Borrower or Book correctly - 3`` () =
+    Library.addItem bk3 bks1 |> should equal bks2
+
+[<Fact>]
+let ``A Library should add a Borrower or Book correctly - 4`` () =
+    Library.addItem bk2 bks1 |> should equal bks1
