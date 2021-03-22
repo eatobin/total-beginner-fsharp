0c5ab923480790cc9c6feb812057e504a347d8fc
Eric Tobin <eatobin@gmail.com>
2021-03-21 21:28:46 -0500

    finditem!!

--- Library.fs
+++ Library.fs
@@ -4,3 +4,9 @@ let addItem x xs =
     if List.contains x xs then xs else x :: xs
 
 let removeBook bk bks = List.filter (fun b -> b <> bk) bks
+
+let findItem (tgt: string) (coll: 'a list) (f: ('a -> string)): 'a option =
+    let res =
+        (List.filter (fun item -> f (item) = tgt) coll)
+
+    if res.IsEmpty then None else Some(res.Head)
