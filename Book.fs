db21f56fbd7f3e443a92f59a8d449940d8ad7e57
Eric Tobin <eatobin@gmail.com>
2021-03-23 10:56:40 -0500

    book progress

--- Book.fs
+++ Book.fs
@@ -1,35 +1,36 @@
 module total.Book
-//
-//open FSharp.Json
-//
-//type Book =
-//    { title: string
-//      author: string
-//      borrower: Borrower.Borrower option }
-//
-//type JsonString = string
-//
-//let defaultBook =
-//    { title = ""
-//      author = ""
-//      borrower = None }
-//
-//let getTitle bk = bk.title
-//
-//let getAuthor bk = bk.author
-//
-//let getBorrower bk = bk.borrower
-//
-//let setBorrower br bk = { bk with borrower = br }
-//
-//let private availableString bk =
-//    match (getBorrower bk) with
-//    | Some br -> sprintf "Checked out to %s" (Borrower.getName br)
-//    | None -> "Available"
-//
-//let toString bk =
-//    sprintf "%s by %s; %s" (getTitle bk) (getAuthor bk) (availableString bk)
-//
-//let jsonStringToBook (bookString: JsonString): Book = Json.deserialize<Book> bookString
-//
-//let bookToJsonString (bk: Book): JsonString = Json.serializeU bk
+
+open Thoth.Json.Net
+
+type Book =
+    { title: string
+      author: string
+      borrower: Borrower.Borrower option }
+
+type JsonString = string
+
+let defaultBook =
+    { title = ""
+      author = ""
+      borrower = None }
+
+let getTitle bk = bk.title
+
+let getAuthor bk = bk.author
+
+let getBorrower bk = bk.borrower
+
+let setBorrower br bk = { bk with borrower = br }
+
+let private availableString bk =
+    match (getBorrower bk) with
+    | Some br -> sprintf "Checked out to %s" (Borrower.getName br)
+    | None -> "Available"
+
+let toString bk =
+    sprintf "%s by %s; %s" (getTitle bk) (getAuthor bk) (availableString bk)
+
+let jsonStringToBook (bookString: JsonString): Result<Book, string> =
+    Decode.Auto.fromString<Book> (bookString)
+
+let bookToJsonString (bk: Book): JsonString = Encode.Auto.toString (0, bk)
