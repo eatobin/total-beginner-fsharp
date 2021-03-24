f426d9c9f87314020ae8572b80636b939c5ae731
eatobin <eatobin@gmail.com>
2021-03-23 21:40:28 -0500

    clean up parens

--- Library.fs
+++ Library.fs
@@ -7,7 +7,7 @@ let removeBook bk bks = List.filter (fun b -> b <> bk) bks
 
 let findItem (tgt: string) (coll: 'a list) (f: ('a -> string)): 'a option =
     let res =
-        (List.filter (fun item -> f (item) = tgt) coll)
+        List.filter (fun item -> f (item) = tgt) coll
 
     if res.IsEmpty then None else Some(res.Head)
 
