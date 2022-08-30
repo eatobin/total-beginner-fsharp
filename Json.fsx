ac1c44c87c8b61f3a0a40718f79748279df90018
Eric Tobin <eatobin@gmail.com>
2022-08-30 12:02:13 -0500

    more json with option

--- Json.fsx
+++ Json.fsx
@@ -1,14 +1,23 @@
 #r "nuget: FSharp.Json"
+
+open System
 open FSharp.Json
 
 // Your record type
 type RecordType =
     { stringMember: string
-      intMember: int }
+      intMember: int
+      boolOptMember: Boolean option }
 
 let data: RecordType =
     { stringMember = "The string"
-      intMember = 123 }
+      intMember = 123
+      boolOptMember = None }
+
+let data2: RecordType =
+    { stringMember = "The string"
+      intMember = 123
+      boolOptMember = Some(false) }
 
 // serialize record into JSON
 let json = Json.serialize data
@@ -20,4 +29,15 @@ let deserialized =
     Json.deserialize<RecordType> json
 
 printfn $"\ndeserialized is:\n%A{deserialized}"
+// deserialized is {stringMember = "some value"; intMember = 123;}
+
+let json2 = Json.serialize data2
+printfn $"\njson2 is:\n %s{json2}"
+// json is """{ "stringMember": "The string", "intMember": 123 }"""
+
+// deserialize from JSON to record
+let deserialized2 =
+    Json.deserialize<RecordType> json2
+
+printfn $"\ndeserialized2 is:\n%A{deserialized2}"
 // deserialized is {stringMember = "some value"; intMember = 123;}
