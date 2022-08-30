7a817fd60b36ed80dc7a7da733e848f3e14f96af
Eric Tobin <eatobin@gmail.com>
2022-08-30 12:17:35 -0500

    json cleanup

--- Json.fsx
+++ Json.fsx
@@ -15,29 +15,29 @@ let data: RecordType =
       boolOptMember = None }
 
 let data2: RecordType =
-    { stringMember = "The string"
-      intMember = 123
-      boolOptMember = Some(false) }
+    { stringMember = "The string2"
+      intMember = 456
+      boolOptMember = Some false }
 
 // serialize record into JSON
 let json = Json.serialize data
 printfn $"\njson is:\n %s{json}"
-// json is """{ "stringMember": "The string", "intMember": 123 }"""
+// json is """{ "stringMember": "The string", "intMember": 123, "boolOptMember": null }"""
 
 // deserialize from JSON to record
 let deserialized =
     Json.deserialize<RecordType> json
 
 printfn $"\ndeserialized is:\n%A{deserialized}"
-// deserialized is {stringMember = "some value"; intMember = 123;}
+// deserialized is {stringMember = "some value"; intMember = 123; boolOptMember = None}
 
 let json2 = Json.serialize data2
 printfn $"\njson2 is:\n %s{json2}"
-// json is """{ "stringMember": "The string", "intMember": 123 }"""
+// json2 is """{ "stringMember": "The string2", "intMember": 456, "boolOptMember": false }"""
 
 // deserialize from JSON to record
 let deserialized2 =
     Json.deserialize<RecordType> json2
 
 printfn $"\ndeserialized2 is:\n%A{deserialized2}"
-// deserialized is {stringMember = "some value"; intMember = 123;}
+// deserialized2 is {stringMember = "some value"; intMember = 123; boolOptMember = Some false}
